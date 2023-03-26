ddbd048e69f776c76ded3c21ee0bf8e6bc21c254
Timur Tuleuov <timmmur123@gmail.com>
2023-03-26 17:47:57 +0600

    upgrade landing page

---
+++ src/app/components/potential/potential.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-potential',
+  templateUrl: './potential.component.html',
+  styleUrls: ['./potential.component.css']
+})
+export class PotentialComponent {
+
+}
